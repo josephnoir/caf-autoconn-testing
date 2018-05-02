ff2c590ba99a3289c1e6531564b38983b55cf351
Joseph Noir <joseph.noir@gmail.com>
2018-05-02 17:08:16 +0200

    Imporve caf build script to use more cores

--- setup.sh
+++ setup.sh
@@ -2,9 +2,25 @@
 
 ROOT_DIR=$(pwd)
 
-git clone https://github.com/actor-framework/actor-framework.git
-cd actor-framework
+cores=4
+if [ "$(uname)" == "Darwin" ]; then
+  cores=$(sysctl -n hw.ncpu)
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  cores=$(nproc --all)
+fi
+
+echo "Using $cores cores for compilation."
+
+# This only works with newer git versions:
+# git -C actor-framework pull || git clone https://github.com/actor-framework/actor-framework.git
+if cd actor-framework
+then
+  git pull
+else 
+  git clone https://server/repo repo
+fi
+cd $ROOT_DIR/actor-framework
 git checkout topic/improve-autoconnect
 ./configure --build-type=release --no-opencl --no-tools --no-examples
-make -j4
+make -j$cores
 cd $ROOT_DIR
