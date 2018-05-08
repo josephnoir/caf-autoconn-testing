5c24f2b3e40340cbdd47d81140b941f6c3e8c81d
Joseph Noir <joseph.noir@gmail.com>
2018-05-08 13:46:40 +0200

    Make the test program more robust

--- src/ping.cpp
+++ src/ping.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <iomanip>
 #include <iostream>
 
 #include <caf/all.hpp>
@@ -9,7 +10,9 @@ using namespace caf::io;
 
 namespace {
 
+using done_atom = caf::atom_constant<atom("done")>;
 using ping_atom = caf::atom_constant<atom("ping")>;
+using pong_atom = caf::atom_constant<atom("pong")>;
 using share_atom = caf::atom_constant<atom("share")>;
 using shutdown_atom = caf::atom_constant<atom("shutdown")>;
 
@@ -30,6 +33,7 @@ public:
   uint16_t local_port = 0;
   uint16_t offset = 0;
   uint32_t others = 7;
+  bool leader = false;
   configuration() {
     load<io::middleman>();
     set("middleman.enable-tcp", true);
@@ -39,63 +43,67 @@ public:
       .add(local_port, "local-port,L", "set local port")
       .add(host, "host,H", "set host")
       .add(offset, "offset,O", "set offset for ports (for repeated local testing)")
+      .add(leader, "leader,L", "make this node the leader")
       .add(others, "others,o", "set number of other nodes");
   }
 };
 
 struct cache {
   actor next;
-  uint32_t received_pings;
-  uint32_t sent_pings;
-  system_clock::time_point start;
+  uint32_t received_pongs;
+  bool done;
 };
 
-behavior ping_test(stateful_actor<cache>* self, uint32_t other_nodes) {
-  self->state.received_pings = 0;
-  self->state.sent_pings = 0;
+behavior ping_test(stateful_actor<cache>* self, uint32_t other_nodes, bool leader) {
+  self->state.received_pongs = 0;
+  self->set_default_handler(skip);
   return {
     [=](actor next) {
-      std::cout << "[+] " << to_string(next.node()) << std::endl;
-      self->state.start = system_clock::now();
+      std::cout << "[+] " << next.node().process_id() << std::endl;
       self->state.next = next;
       self->send(next, share_atom::value, self);
-    },
-    [=](share_atom, actor an_actor) {
-      auto&s = self->state;
-      if (an_actor == self) {
-        std::cout << "[!] actor returned" << std::endl;
-        auto dur = system_clock::now() - s.start;
-        std::cout << "Got my actor back after "
-                  << duration_cast<milliseconds>(dur).count()
-                  << std::endl;
-      } else {
-        std::cout << "[+] " << to_string(an_actor.node()) << std::endl;
-        self->send(s.next, share_atom::value, an_actor);
-        self->send(an_actor, ping_atom::value);
-        s.sent_pings += 1;
-        if (s.sent_pings >= other_nodes && s.received_pings >= other_nodes) {
-          auto dur = system_clock::now() - self->state.start;
-          std::cout << "Quitting after "
-                    << duration_cast<milliseconds>(dur).count()
-                    << std::endl;
-          self->delayed_send(self, delay, shutdown_atom::value);
+      self->set_default_handler(print_and_drop);
+      self->become(
+        [=](share_atom, actor an_actor) {
+          auto&s = self->state;
+          if (an_actor == self) {
+            std::cout << "[!] actor returned" << std::endl;
+          } else {
+            std::cout << "[+] " << an_actor.node().process_id() << std::endl;
+            self->send(s.next, share_atom::value, an_actor);
+            self->send(an_actor, ping_atom::value);
+          }
+        },
+        [=](ping_atom) {
+          std::cout << "[>] " << self->current_sender()->node().process_id() << std::endl;
+          return pong_atom::value;
+        },
+        [=](pong_atom) {
+          std::cout << "[o] " << self->current_sender()->node().process_id() << std::endl;
+          auto&s = self->state;
+          s.received_pongs += 1;
+          if (s.received_pongs >= other_nodes) {
+            if (leader)
+              self->send(s.next, done_atom::value);
+            else if (s.done)
+              self->send(s.next, done_atom::value);
+          }
+        },
+        [=](done_atom) {
+          std::cout << "[-] " << self->current_sender()->node().process_id() << std::endl;
+          auto&s = self->state;
+          s.done = true;
+          if (leader)
+            self->send(s.next, shutdown_atom::value);
+          else if (s.received_pongs >= other_nodes)
+            self->send(s.next, done_atom::value);
+        },
+        [=](shutdown_atom) {
+          if (!leader)
+            self->send(self->state.next, shutdown_atom::value);
+          self->quit();
         }
-      }
-    },
-    [=](ping_atom) {
-      std::cout << "[>] " << to_string(self->current_sender()->node()) << std::endl;
-      auto&s = self->state;
-      s.received_pings += 1;
-      if (s.received_pings >= other_nodes && s.sent_pings >= other_nodes) {
-        auto dur = system_clock::now() - self->state.start;
-        std::cout << "Quitting after "
-                  << duration_cast<milliseconds>(dur).count()
-                  << std::endl;
-        self->delayed_send(self, delay, shutdown_atom::value);
-      }
-    },
-    [=](shutdown_atom) {
-      self->quit();
+      );
     }
   };
 }
@@ -108,30 +116,41 @@ void caf_main(actor_system& system, const configuration& config) {
             << ", local-port = " << config.local_port
             << ", others = " << config.others
             << ", offset = " << config.offset
+            << ", leader = " << std::boolalpha << config.leader
             << std::endl;
   auto remote_port = config.port + config.offset;
   auto local_port = config.local_port + config.offset;
   if (local_port == 0)
     local_port = remote_port;
-  std::cout << "System on node " << to_string(system.node()) << std::endl;
+  std::cout << "Node id: " << system.node().process_id() << std::endl;
   scoped_actor self{system};
-  auto pt = system.spawn(ping_test, config.others);
+  auto pt = system.spawn(ping_test, config.others, config.leader);
+
+  std::cout << std::endl << "Opening local port ... " << std::endl;
   auto port = system.middleman().publish(pt, local_port);
   if (!port) {
     std::cerr << "Could not publish my actor on port " << local_port
               << std::endl;
     return;
   }
+  std::cout << "Published actor on " << *port << std::endl;
+
   self->delayed_send(self, delay, ping_atom::value);
-  self->receive([&](ping_atom) { std::cout << "Acquiring remote actor." << std::endl; });
+  self->receive([&](ping_atom) {
+    std::cout << std::endl << "Connecting to next node ..." << std::endl;
+  });
   auto next = system.middleman().remote_actor(config.host, remote_port);
   if (!next) {
     std::cerr << "Could not connect to next node! (" << config.host << ":"
               << remote_port << ")" << std::endl;
     return;
   }
+  std::cout << "Connected." << std::endl;
+
   self->delayed_send(self, delay, ping_atom::value);
-  self->receive([&](ping_atom) { std::cout << "Let's do this!" << std::endl; });
+  self->receive([&](ping_atom) {
+    std::cout << std::endl << "Starting interaction ..." << std::endl;
+  });
   self->send(pt, *next);
 }
 
