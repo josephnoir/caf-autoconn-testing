b0b16733df2f93ba79e8d18a1fd44107e345d977
Joseph Noir <joseph.noir@gmail.com>
2018-05-24 16:33:58 +0200

    Add support for udp and node names

--- src/ping.cpp
+++ src/ping.cpp
@@ -23,6 +23,7 @@ using shutdown_atom = caf::atom_constant<atom("shutdown")>;
 class configuration : public actor_system_config {
 public:
   std::string host = "localhost";
+  std::string name = "";
   uint16_t port = 12345;
   uint16_t local_port = 0;
   uint16_t offset = 0;
@@ -31,73 +32,70 @@ public:
   bool leader = false;
   configuration() {
     load<io::middleman>();
-    set("middleman.enable-tcp", true);
-    set("middleman.enable-udp", false);
-    opt_group{custom_options_, "global"}
-      .add(port, "port,P", "set remote port")
+    opt_group{custom_options_,         "global"}
+      .add(port,       "port,P",       "set remote port")
       .add(local_port, "local-port,L", "set local port")
-      .add(host, "host,H", "set host")
-      .add(offset, "offset,O", "set offset for ports (for repeated local testing)")
-      .add(leader, "leader,L", "make this node the leader")
-      .add(timeout, "timeout,t", "use a timeout (sec) instead of user input")
-      .add(others, "others,o", "set number of other nodes");
+      .add(host,       "host,H",       "set host")
+      .add(offset,     "offset,O",     "set offset for ports (for repeated local testing)")
+      .add(leader,     "leader,L",     "make this node the leader")
+      .add(timeout,    "timeout,t",    "use a timeout (sec) instead of user input")
+      .add(name,       "name,n",       "name used for debugging")
+      .add(others,     "others,o",     "set number of other nodes");
   }
 };
 
 struct cache {
   actor next;
   uint32_t received_pongs;
-  bool done;
+  bool received_done;
 };
 
-behavior ping_test(stateful_actor<cache>* self, uint32_t other_nodes, bool leader) {
+behavior ping_test(stateful_actor<cache>* self, uint32_t other_nodes, bool leader, const std::string& my_name) {
   self->state.received_pongs = 0;
   self->set_default_handler(skip);
   return {
     [=](actor next) {
-      std::cout << "[+] " << next.node().process_id() << std::endl;
+      std::cout << "[n] " << next.node().process_id() << std::endl;
       self->state.next = next;
-      self->send(next, share_atom::value, self);
+      self->send(next, share_atom::value, self, my_name);
       self->set_default_handler(print_and_drop);
       self->become(
-        [=](share_atom, actor an_actor) {
+        [=](share_atom, actor an_actor, const std::string& name) {
           auto&s = self->state;
           if (an_actor == self) {
-            std::cout << "[!] actor returned" << std::endl;
+            std::cout << "[r] actor returned" << std::endl;
           } else {
-            std::cout << "[+] " << an_actor.node().process_id() << std::endl;
-            self->send(s.next, share_atom::value, an_actor);
-            self->send(an_actor, ping_atom::value);
+            std::cout << "[s] " << name << std::endl;
+            self->send(s.next, share_atom::value, an_actor, name);
+            self->send(an_actor, ping_atom::value, my_name);
           }
         },
-        [=](ping_atom) {
-          std::cout << "[>] " << self->current_sender()->node().process_id() << std::endl;
-          return pong_atom::value;
+        [=](ping_atom, const std::string& name) {
+          std::cout << "[i] " << name << std::endl;
+          return caf::make_message(pong_atom::value, my_name);
         },
-        [=](pong_atom) {
-          std::cout << "[o] " << self->current_sender()->node().process_id() << std::endl;
+        [=](pong_atom, const std::string& name) {
+          std::cout << "[o] " << name << std::endl;
           auto&s = self->state;
           s.received_pongs += 1;
           if (s.received_pongs >= other_nodes) {
-            std::cout << "[ö] got answers from all others" << std::endl;
-            if (leader)
-              self->send(s.next, done_atom::value);
-            else if (s.done)
-              self->send(s.next, done_atom::value);
+            std::cout << "[O] got answers from all others" << std::endl;
+            if (leader || s.received_done)
+              self->send(s.next, done_atom::value, name);
           }
         },
-        [=](done_atom) {
-          std::cout << "[-] " << self->current_sender()->node().process_id() << std::endl;
+        [=](done_atom, const std::string& name) {
+          std::cout << "[d] " << name << std::endl;
           auto&s = self->state;
-          s.done = true;
+          s.received_done = true;
           if (leader)
-            self->send(s.next, shutdown_atom::value);
+            self->send(s.next, shutdown_atom::value, name);
           else if (s.received_pongs >= other_nodes)
-            self->send(s.next, done_atom::value);
+            self->send(s.next, done_atom::value, name);
         },
-        [=](shutdown_atom) {
+        [=](shutdown_atom, const std::string& name) {
           if (!leader)
-            self->send(self->state.next, shutdown_atom::value);
+            self->send(self->state.next, shutdown_atom::value, name);
           self->quit();
         }
       );
@@ -107,24 +105,57 @@ behavior ping_test(stateful_actor<cache>* self, uint32_t other_nodes, bool leade
 
 } // namespace anonymous
 
+
+
+struct net_stuff {
+  net_stuff(actor_system& sys, const configuration& config)
+    : sys(sys), config(config) {
+    // nop
+  }
+
+  template <class ...Ts>
+  auto remote_actor(Ts&&... args) {
+    if (config.middleman_enable_udp)
+      return sys.middleman().remote_actor_udp(std::forward<Ts>(args)...);
+    else
+      return sys.middleman().remote_actor(std::forward<Ts>(args)...);
+  }
+
+  template <class ...Ts>
+  auto publish(Ts&&... args) {
+    if (config.middleman_enable_udp)
+      return sys.middleman().publish_udp(std::forward<Ts>(args)...);
+    else
+      return sys.middleman().publish(std::forward<Ts>(args)...);
+  }
+
+  actor_system& sys;
+  const configuration& config;
+};
+
 void caf_main(actor_system& system, const configuration& config) {
   std::cout << "Config: \n > host = " << config.host << std::endl
             << " > port = " << config.port << std::endl
             << " > local-port = " << config.local_port << std::endl
             << " > others = " << config.others << std::endl
             << " > offset = " << config.offset << std::endl
-            << " > leader = " << std::boolalpha << config.leader  << std::endl
-            << " > timeout = " << config.timeout << std::endl;
+            << " > leader = " << std::boolalpha << config.leader << std::endl
+            << " > udp = " << std::boolalpha << config.middleman_enable_udp << std::endl
+            << " > tcp = " << std::boolalpha << config.middleman_enable_tcp << std::endl
+            << " > timeout = " << config.timeout << std::endl
+            << " > name = " << config.name << std::endl;
+  net_stuff ns(system, config);
   auto remote_port = config.port + config.offset;
   auto local_port = config.local_port + config.offset;
+  auto name = config.name.empty() ? std::to_string(system.node().process_id()) : config.name;
   if (config.local_port == 0)
     local_port = remote_port;
-  std::cout << "Node id: " << system.node().process_id() << std::endl;
+  std::cout << "Node name = " << name << ", id = " << system.node().process_id() << std::endl;
   scoped_actor self{system};
-  auto pt = system.spawn(ping_test, config.others, config.leader);
+  auto pt = system.spawn(ping_test, config.others, config.leader, name);
 
   std::cout << std::endl << "Opening local port ... " << std::endl;
-  auto port = system.middleman().publish(pt, local_port, nullptr, true);
+  auto port = ns.publish(pt, local_port, nullptr, true);
   if (!port) {
     std::cerr << "Could not publish my actor on port " << local_port
               << std::endl;
@@ -147,7 +178,7 @@ void caf_main(actor_system& system, const configuration& config) {
     std::cin.get();
     std::cout << std::endl << "Connecting to next node ..." << std::endl;
   }
-  auto next = system.middleman().remote_actor(config.host, remote_port);
+  auto next = ns.remote_actor(config.host, remote_port);
   if (!next) {
     std::cerr << "Could not connect to next node! (" << config.host << ":"
               << remote_port << ")" << std::endl;
